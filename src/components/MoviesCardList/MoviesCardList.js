2eede4585dc56979d9be01cdf7f8bc5b8b661b1a
Alexashka-2022 <avibus.bonis.2022@gmail.com>
2023-07-27 14:57:05 +0300

    Исправлены замечания ревьювера

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,7 +1,9 @@
 import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
+import { useLocation } from 'react-router-dom';
 
 function MoviesCardList(props) {
+    const { pathname } = useLocation();
     return (
         <section className="movies-list">
             <ul className="movies-list__container">
@@ -9,7 +11,10 @@ function MoviesCardList(props) {
                     <MoviesCard key={index} />
                 ))}
             </ul>
-            <button className="movies-list__add-more" type="button">Ещё</button>
+            {pathname === "/movies" ?
+                (<button className="movies-list__add-more" type="button">Ещё</button>)
+                :
+                (<div className="movies-list__empty-place" />)}
         </section>
     );
 }
